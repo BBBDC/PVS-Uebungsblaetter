263aba92729cf012cd1f96ebd4ab4f0acd103e1d
Jonas <Jonas@134.60.182.183>
2018-06-06 11:09:34 +0200

    ihih

--- Blatt7_Maximilian_mayer/src/mayer/maximilian/sheet07/exercise01/Car.java
+++ Blatt7_Maximilian_mayer/src/mayer/maximilian/sheet07/exercise01/Car.java
@@ -1,7 +1,7 @@
 package mayer.maximilian.sheet07.exercise01;
 
 import java.io.Serializable;
-/*geigjiegj*/
+/*ege*/
 public class Car implements Serializable{
 	
 	private static long serialVersionUID;
